c018d9f7f1fe73cf3ba23a206730b910c82b0b04
Justman100 <149507651+Justman100@users.noreply.github.com>
2025-02-11 22:08:07 +0100

    Add files via upload

---
+++ mediawiki.js
@@ -0,0 +1,24 @@
+// Externally
+mw.loader.load("https://deadlinkchecker.toolforge.org/static/script.js");
+
+// Other wikis
+mw.loader.load('https://testwiki.wiki/index.php?title=MediaWiki:Gadget-UserInfo.js&action=raw&ctype=text/javascript');
+
+// MediaWiki
+mw.loader.load('https://www.mediawiki.org/w/index.php?title=XTools/ArticleInfo.js&action=raw&ctype=text/javascript');
+
+// Meta
+mw.loader.load('https://meta.wikimedia.org/w/index.php?title=MediaWiki:Gadget-markblocked.js&action=raw&ctype=text/javascript');
+mw.loader.load('https://meta.wikimedia.org/w/index.php?title=MediaWiki:MoreMenu.import.js&action=raw&ctype=text/javascript');
+
+// English Wikipedia
+mw.loader.load('https://en.wikipedia.org/w/index.php?title=User:GeneralNotability/spihelper.js&action=raw&ctype=text/javascript');
+mw.loader.load('https://en.wikipedia.org/w/index.php?title=User:BrandonXLF/EasySummary.js&action=raw&ctype=text/javascript');
+mw.loader.load('https://en.wikipedia.org/w/index.php?title=User:BrandonXLF/Restorer.js&action=raw&ctype=text/javascript');
+mw.loader.load('https://en.wikipedia.org/w/index.php?title=User:BrandonXLF/Subpages.js&action=raw&ctype=text/javascript');
+mw.loader.load('https://en.wikipedia.org/w/index.php?title=User:BrandonXLF/ShowRevisionID.js&action=raw&ctype=text/javascript');
+mw.loader.load('https://en.wikipedia.org/w/index.php?title=User:BrandonXLF/QuickEdit.js&action=raw&ctype=text/javascript');
+// mw.loader.load('https://en.wikipedia.org/w/index.php?title=User:BrandonXLF/ContribsByPage.js&action=raw&ctype=text/javascript');
+
+// German Wikipedia
+mw.loader.load('https://de.wikipedia.org/w/index.php?title=User:TenWhile6/XReport.js&action=raw&ctype=text/javascript');
